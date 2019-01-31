d5dc587f650adda1e5b6cc56750028b2871ef34b
Tom Goring <tgoring97@gmail.com>
2019-01-31 20:26:53 +0000

    Expand Team class functionality and remove TODO

--- AssociationOrganiser/src/Team.java
+++ AssociationOrganiser/src/Team.java
@@ -1,12 +1,35 @@
-// TODO: expand Team class
 public class Team {
 
     private int teamID;
     private String teamName;
 
-    public Team(int teamID, String teamName) {
+    Team(int teamID, String teamName) {
 
         this.teamID = teamID;
         this.teamName = teamName;
+    }
+
+    public int getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(int teamID) {
+        this.teamID = teamID;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamID=" + teamID +
+                ", teamName='" + teamName + '\'' +
+                '}';
     }
 }

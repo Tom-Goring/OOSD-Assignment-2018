e3bd09e20bc2cad29c0afb1a38348c27fd409bd0
Tom Goring <tgoring97@gmail.com>
2019-01-31 21:08:27 +0000

    Update test main to createMatch() testing

--- AssociationOrganiser/src/Test.java
+++ AssociationOrganiser/src/Test.java
@@ -6,14 +6,23 @@ public class Test {
         DatabaseManager.createTables();
 
         DatabaseManager.addTeamToDatabase("UWE");
+        DatabaseManager.addTeamToDatabase("Page");
+        DatabaseManager.addTeamToDatabase("Filton");
 
         DatabaseManager.addPlayerToDatabase("Tom", "UWE");
         DatabaseManager.addPlayerToDatabase("Louis", "UWE");
         DatabaseManager.addPlayerToDatabase("Tim", "UWE");
+        DatabaseManager.addPlayerToDatabase("Bob", "Page");
+        DatabaseManager.addPlayerToDatabase("Jun", "Page");
 
-        for (Player player : DatabaseManager.getPlayersWithTeamName("UWE")) {
+        DatabaseManager.createMatch(
+                DatabaseManager.getTeamWithName("UWE").getTeamID(), // Home Team
+                DatabaseManager.getTeamWithName("Page").getTeamID(), // Away Team
+                DatabaseManager.getPlayerWithName("Tom").getPlayer_id(), // HP1
+                DatabaseManager.getPlayerWithName("Louis").getPlayer_id(), // HP2
+                DatabaseManager.getPlayerWithName("Bob").getPlayer_id(), // AP1
+                DatabaseManager.getPlayerWithName("Jun").getPlayer_id() ); // AP2
 
-            System.out.println(player.toString());
-        }
+        System.out.println("Pause here.");
     }
 }

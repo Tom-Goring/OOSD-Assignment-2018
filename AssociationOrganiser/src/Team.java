f7f20fca29ebbd02678db3090a8597dbf7f45695
Tom Goring <tgoring97@gmail.com>
2019-01-31 19:22:27 +0000

    Add basic Team class

--- AssociationOrganiser/src/Team.java
+++ AssociationOrganiser/src/Team.java
@@ -1,3 +1,12 @@
-// TODO: add Team class
+// TODO: expand Team class
 public class Team {
+
+    private int teamID;
+    private String  teamName;
+
+    public Team(int teamID, String teamName) {
+
+        this.teamID = teamID;
+        this.teamName = teamName;
+    }
 }

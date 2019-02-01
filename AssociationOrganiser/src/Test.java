c513dea6c74f334302466a98870b508afb095e05
Tom Goring <tgoring97@gmail.com>
2019-02-01 14:02:08 +0000

    Add testing of generateFixtures method (implicit test of createMatch)

--- AssociationOrganiser/src/Test.java
+++ AssociationOrganiser/src/Test.java
@@ -15,13 +15,7 @@ public class Test {
         DatabaseManager.addPlayerToDatabase("Bob", "Page");
         DatabaseManager.addPlayerToDatabase("Jun", "Page");
 
-        DatabaseManager.createMatch(
-                DatabaseManager.getTeamWithName("UWE").getTeamID(), // Home Team
-                DatabaseManager.getTeamWithName("Page").getTeamID(), // Away Team
-                DatabaseManager.getPlayerWithName("Tom").getPlayer_id(), // HP1
-                DatabaseManager.getPlayerWithName("Louis").getPlayer_id(), // HP2
-                DatabaseManager.getPlayerWithName("Bob").getPlayer_id(), // AP1
-                DatabaseManager.getPlayerWithName("Jun").getPlayer_id() ); // AP2
+        DatabaseManager.generateFixtures();
 
         System.out.println("Pause here.");
     }

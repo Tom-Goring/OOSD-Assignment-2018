a0f622f792b5f962aab025e9c5526e1b30299958
Tom Goring <tgoring97@gmail.com>
2019-02-01 18:19:52 +0000

    Update TODOs

--- AssociationOrganiser/src/Fixtures.java
+++ AssociationOrganiser/src/Fixtures.java
@@ -1,8 +1,6 @@
-// TODO: add Fixtures class
 public class Fixtures {
 //Woah //you made me merge u bastard
 
-    // TODO: have the code to generate all the matches here?
     // TODO: finish generateFixtures
     static void generateFixtures() {
 

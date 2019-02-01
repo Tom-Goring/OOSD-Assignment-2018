a5c15a5df93e263096e8b3a94c4d1400c74ba3e8
Tom Goring <tgoring97@gmail.com>
2019-02-01 18:29:10 +0000

    Add getters and setters to Match

--- AssociationOrganiser/src/Match.java
+++ AssociationOrganiser/src/Match.java
@@ -3,20 +3,25 @@ import java.util.ArrayList;
 // TODO: add Match class
 public class Match {
 
-    int HomeTeamID;
-    String HomeTeamName;
+    private int HomeTeamID;
+    private String HomeTeamName;
 
-    int AwayTeamID;
-    String AwayTeamName;
+    private int AwayTeamID;
+    private String AwayTeamName;
 
-    ArrayList<Set> setList;
+    private ArrayList<Set> setList;
 
-    int WinnerID;
-    int WinnerName;
+    private String finalScore;
+
+    private int WinnerID;
+    private int WinnerName;
 
     Match(int HomeTeamID, String HomeTeamName, int AwayTeamID, String AwayTeamName) {
 
-        // TODO: finish constructor for Match
+        this.HomeTeamID = HomeTeamID;
+        this.HomeTeamName = HomeTeamName;
+        this.AwayTeamID = AwayTeamID;
+        this.AwayTeamName = AwayTeamName;
     }
 
     static void createMatch(int HTID, int ATID) {
@@ -31,5 +36,69 @@ public class Match {
         DatabaseManager.insertData(insert);
 
         // createMatchSets
+    }
+
+    public int getHomeTeamID() {
+        return HomeTeamID;
+    }
+
+    public void setHomeTeamID(int homeTeamID) {
+        HomeTeamID = homeTeamID;
+    }
+
+    public String getHomeTeamName() {
+        return HomeTeamName;
+    }
+
+    public void setHomeTeamName(String homeTeamName) {
+        HomeTeamName = homeTeamName;
+    }
+
+    public int getAwayTeamID() {
+        return AwayTeamID;
+    }
+
+    public void setAwayTeamID(int awayTeamID) {
+        AwayTeamID = awayTeamID;
+    }
+
+    public String getAwayTeamName() {
+        return AwayTeamName;
+    }
+
+    public void setAwayTeamName(String awayTeamName) {
+        AwayTeamName = awayTeamName;
+    }
+
+    public ArrayList<Set> getSetList() {
+        return setList;
+    }
+
+    public void setSetList(ArrayList<Set> setList) {
+        this.setList = setList;
+    }
+
+    public String getFinalScore() {
+        return finalScore;
+    }
+
+    public void setFinalScore(String finalScore) {
+        this.finalScore = finalScore;
+    }
+
+    public int getWinnerID() {
+        return WinnerID;
+    }
+
+    public void setWinnerID(int winnerID) {
+        WinnerID = winnerID;
+    }
+
+    public int getWinnerName() {
+        return WinnerName;
+    }
+
+    public void setWinnerName(int winnerName) {
+        WinnerName = winnerName;
     }
 }

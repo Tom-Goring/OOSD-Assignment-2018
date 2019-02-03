7b8dfac6f54ce90b7a6883e10568d39e6c57a1f8
Tom Goring <tgoring97@gmail.com>
2019-02-03 20:30:01 +0000

    Fill out Match attributes and add updateMatchPlayers()

--- AssociationOrganiser/src/Match.java
+++ AssociationOrganiser/src/Match.java
@@ -1,6 +1,62 @@
+import java.util.ArrayList;
+
 public class Match {
 
     // TODO: add instance and class variables + methods for Match class
+
+    private String HomeTeamName;
+    private String AwayTeamName;
+
+    // TODO: consider making these actual player classes
+    private String HomePlayer1;
+    private String HomePlayer2;
+    private String AwayPlayer1;
+    private String AwayPlayer2;
+
+    int HomeTeamSetsWon;
+    int AwayTeamSetsWon;
+
+    String WinningTeam;
+
+    public Match(String homeTeamName, String awayTeamName,
+                 String homePlayer1, String homePlayer2,
+                 String awayPlayer1, String awayPlayer2) {
+
+        HomeTeamName = homeTeamName;
+        AwayTeamName = awayTeamName;
+        HomePlayer1 = homePlayer1;
+        HomePlayer2 = homePlayer2;
+        AwayPlayer1 = awayPlayer1;
+        AwayPlayer2 = awayPlayer2;
+    }
+
+    // Placeholder
+    public Match(String homeTeamName, String awayTeamName) {
+
+        this.HomeTeamName = homeTeamName;
+        this.AwayTeamName = awayTeamName;
+
+        // TODO: consider adding constructor code that pulls relevant match from database
+    }
+
+    // TODO: finish updateMatchPlayers - add proper WHERE statement
+    public void updateMatchPlayers() {
+
+        String WHERE = "WHERE (HomeTeamName = " + DatabaseManager.surroundWithQuotes(this.HomeTeamName) + " AND "
+                + "AwayTeamName = " + DatabaseManager.surroundWithQuotes(this.AwayTeamName);
+
+        ArrayList<String> commandList = new ArrayList<>();
+
+        commandList.add("UPDATE `Match` SET HomePlayer1ID = " + this.HomePlayer1 + ";");
+        commandList.add("UPDATE `Match` SET HomePlayer2ID = " + this.HomePlayer2 + ";");
+        commandList.add("UPDATE `Match` SET AwayPlayer1ID = " + this.AwayPlayer1 + ";");
+        commandList.add("UPDATE `Match` SET AwayPlayer2ID = " + this.AwayPlayer2 + ";");
+
+        for (String command : commandList) {
+
+            DatabaseManager.insertData(command);
+        }
+    }
 
     /**********************************************STATIC METHODS******************************************************/
 
@@ -33,5 +89,30 @@ public class Match {
         insert += getHTID + "," + getATID + ");";
 
         DatabaseManager.insertData(insert);
+    }
+
+    static Match getMatchWithTeams(String homeTeamName, String awayTeamName) {
+
+        // TODO: add getMatchWithTeams()
+
+        return new Match(homeTeamName, awayTeamName);
+    }
+
+    /**********************************************GETTERS + SETTERS*******************************************************/
+
+    public void setHomePlayer1(String homePlayer1) {
+        HomePlayer1 = homePlayer1;
+    }
+
+    public void setHomePlayer2(String homePlayer2) {
+        HomePlayer2 = homePlayer2;
+    }
+
+    public void setAwayPlayer1(String awayPlayer1) {
+        AwayPlayer1 = awayPlayer1;
+    }
+
+    public void setAwayPlayer2(String awayPlayer2) {
+        AwayPlayer2 = awayPlayer2;
     }
 }
--- AssociationOrganiser/src/Test.java
+++ AssociationOrganiser/src/Test.java
@@ -7,13 +7,26 @@ public class Test {
 
         Team.addTeamToDatabase("UWE");
         Team.addTeamToDatabase("Page");
+        Team.addTeamToDatabase("Filton");
 
         Player.addPlayerToDatabase("Tom", "UWE");
         Player.addPlayerToDatabase("Louis", "UWE");
         Player.addPlayerToDatabase("Bob", "Page");
         Player.addPlayerToDatabase("Jun", "Page");
+        Player.addPlayerToDatabase("John", "Filton");
+        Player.addPlayerToDatabase("Frank", "Filton");
 
         Fixtures.generateFixtures();
+
+        Match match = new Match("UWE" , "Page");
+
+        match.setHomePlayer1("Tom");
+        match.setHomePlayer1("Louis");
+        match.setAwayPlayer1("Bob");
+        match.setAwayPlayer2("Jun");
+
+        match.updateMatchPlayers();
+
         System.out.println("Pause here.");
     }
 }

802da1f11281205fafcacd8c05eb01ef86f0c7a5
Tom Goring <tgoring97@gmail.com>
2019-02-03 17:02:49 +0000

    Remove unused getters, setters, and constructor from Match class

--- AssociationOrganiser/src/Match.java
+++ AssociationOrganiser/src/Match.java
@@ -3,23 +3,6 @@ import java.util.ArrayList;
 // TODO: add Match class
 public class Match {
 
-    private int HomeTeamID;
-    private String HomeTeamName;
-    private int AwayTeamID;
-    private String AwayTeamName;
-    private ArrayList<Set> setList;
-    private String finalScore;
-    private int WinnerID;
-    private int WinnerName;
-
-    Match(int HomeTeamID, String HomeTeamName, int AwayTeamID, String AwayTeamName) {
-
-        this.HomeTeamID = HomeTeamID;
-        this.HomeTeamName = HomeTeamName;
-        this.AwayTeamID = AwayTeamID;
-        this.AwayTeamName = AwayTeamName;
-    }
-
     static void generateMatches() {
 
         for (Team team_outer : Team.getTeamList()) {
@@ -49,71 +32,5 @@ public class Match {
         insert += getHTID + "," + getATID + ");";
 
         DatabaseManager.insertData(insert);
-    }
-
-
-
-    public int getHomeTeamID() {
-        return HomeTeamID;
-    }
-
-    public void setHomeTeamID(int homeTeamID) {
-        HomeTeamID = homeTeamID;
-    }
-
-    public String getHomeTeamName() {
-        return HomeTeamName;
-    }
-
-    public void setHomeTeamName(String homeTeamName) {
-        HomeTeamName = homeTeamName;
-    }
-
-    public int getAwayTeamID() {
-        return AwayTeamID;
-    }
-
-    public void setAwayTeamID(int awayTeamID) {
-        AwayTeamID = awayTeamID;
-    }
-
-    public String getAwayTeamName() {
-        return AwayTeamName;
-    }
-
-    public void setAwayTeamName(String awayTeamName) {
-        AwayTeamName = awayTeamName;
-    }
-
-    public ArrayList<Set> getSetList() {
-        return setList;
-    }
-
-    public void setSetList(ArrayList<Set> setList) {
-        this.setList = setList;
-    }
-
-    public String getFinalScore() {
-        return finalScore;
-    }
-
-    public void setFinalScore(String finalScore) {
-        this.finalScore = finalScore;
-    }
-
-    public int getWinnerID() {
-        return WinnerID;
-    }
-
-    public void setWinnerID(int winnerID) {
-        WinnerID = winnerID;
-    }
-
-    public int getWinnerName() {
-        return WinnerName;
-    }
-
-    public void setWinnerName(int winnerName) {
-        WinnerName = winnerName;
     }
 }

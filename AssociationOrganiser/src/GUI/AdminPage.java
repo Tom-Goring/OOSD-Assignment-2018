4f0d7029b7d0f0ad789451b5b1fb1ce8858b5075
Louis Harding <Louis2.Harding@live.uwe.ac.uk>
2019-02-04 16:18:05 +0000

    Added GUI libraries

--- AssociationOrganiser/src/GUI/AdminPage.java
+++ AssociationOrganiser/src/GUI/AdminPage.java
@@ -1,5 +1,16 @@
 package GUI;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+
+
 // TODO: make AdminPage GUI page
 public class AdminPage {
 }

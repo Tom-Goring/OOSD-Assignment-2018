a0da8219044787c3b62cf55201ab0bdb41a7bacd
Louis Harding <Louis2.Harding@live.uwe.ac.uk>
2019-02-04 17:21:39 +0000

    Added Flowplane with buttons

--- AssociationOrganiser/src/GUI/AdminPage.java
+++ AssociationOrganiser/src/GUI/AdminPage.java
@@ -5,8 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 
 
 
@@ -16,11 +19,28 @@ public class AdminPage extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Scene scene = new Scene(new Button("TESTING BUTTON"), 200, 200);
-        primaryStage.setTitle("TESTING STAGE");
+        //new pane to work with
+        StackPane pane = new StackPane();
+
+        //create new flowpane to work with
+        FlowPane flowPane = new FlowPane();
+
+        //create new button, add to pane's list of children
+        flowPane.getChildren().add(new Button("OKAY"));
+        flowPane.getChildren().add(new Button("OKAY2"));
+        flowPane.getChildren().add(new Button("OKAY3"));
+
+
+
+
+
+
+        //make new scene, with the pane
+        Scene scene = new Scene(flowPane, 350, 200);
+        flowPane.setHgap(10);
+        primaryStage.setTitle("Flowplanes");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
 

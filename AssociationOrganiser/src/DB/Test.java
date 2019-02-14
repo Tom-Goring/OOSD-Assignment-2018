209b0de1eae066d767933528540fa8153c7ca96c
Tom Goring <tgoring97@gmail.com>
2019-02-14 21:04:26 +0000

    Update test to latest

--- AssociationOrganiser/src/DB/Test.java
+++ AssociationOrganiser/src/DB/Test.java
@@ -7,10 +7,25 @@ public class Test {
 
         DatabaseManager.createTables();
         DatabaseManager.Team.sendTeamToDatabase(new Team("UWE"));
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("Tom", "UWE"));
+        DatabaseManager.Team.sendTeamToDatabase(new Team("Page"));
+        DatabaseManager.Team.sendTeamToDatabase(new Team("Filton"));
 
-        Player tom = DatabaseManager.Player.loadPlayerInformation("Tom");
-        System.out.println(tom.getPlayerName());
-        System.out.println(tom.getTeamName());
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("Tom", "UWE"));
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("Louis", "UWE"));
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("Tim", "UWE"));
+
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI1", "Page"));
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI2", "Page"));
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI3", "Page"));
+
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI4", "Filton"));
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI5", "Filton"));
+        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI6", "Filton"));
+
+        Match UWEvsPage = new Match("UWE", "Page");
+        DatabaseManager.Match.sendNewMatchToDB(UWEvsPage);
+
+        DatabaseManager.Match.updateMatchInformation(UWEvsPage);
+
     }
 }

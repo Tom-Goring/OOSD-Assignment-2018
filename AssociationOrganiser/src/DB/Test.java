238acb8aaad8c5e00238c1530f4b345d92408da8
Tom Goring <tgoring97@gmail.com>
2019-02-15 15:58:20 +0000

    Update Test to latest

--- AssociationOrganiser/src/DB/Test.java
+++ AssociationOrganiser/src/DB/Test.java
@@ -6,26 +6,11 @@ public class Test {
     public static void main(String[] args) {
 
         DatabaseManager.createTables();
-        DatabaseManager.Team.sendTeamToDatabase(new Team("UWE"));
-        DatabaseManager.Team.sendTeamToDatabase(new Team("Page"));
-        DatabaseManager.Team.sendTeamToDatabase(new Team("Filton"));
 
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("Tom", "UWE"));
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("Louis", "UWE"));
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("Tim", "UWE"));
+        Team UWE = new Team("UWE");
+        Team Page = new Team("Page");
+        Team Filton = new Team("Filton");
 
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI1", "Page"));
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI2", "Page"));
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI3", "Page"));
-
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI4", "Filton"));
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI5", "Filton"));
-        DatabaseManager.Player.sendNewPlayerToDB(new Player("AI6", "Filton"));
-
-        Match UWEvsPage = new Match("UWE", "Page");
-        DatabaseManager.Match.sendNewMatchToDB(UWEvsPage);
-
-        DatabaseManager.Match.updateMatchInformation(UWEvsPage);
-
+        Match match = new Match(UWE, Page);
     }
 }

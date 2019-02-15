2e15fa86949856d4c11783ea2aac38cc03faf288
Tom Goring <tgoring97@gmail.com>
2019-02-15 16:27:35 +0000

    Update Test to latest

--- AssociationOrganiser/src/DB/Test.java
+++ AssociationOrganiser/src/DB/Test.java
@@ -9,8 +9,36 @@ public class Test {
 
         Team UWE = new Team("UWE");
         Team Page = new Team("Page");
-        Team Filton = new Team("Filton");
+
+        Player Tom = new Player("Tom", UWE);
+        Player Louis = new Player("Louis", UWE);
+        Player Dave = new Player("Dave", Page);
+        Player Bob = new Player("Bob", Page);
+
+        DatabaseManager.Team.insertTeam(UWE);
+        DatabaseManager.Team.insertTeam(Page);
+
+        DatabaseManager.Player.sendNewPlayerToDB(Tom);
+        DatabaseManager.Player.sendNewPlayerToDB(Louis);
+        DatabaseManager.Player.sendNewPlayerToDB(Tom);
+        DatabaseManager.Player.sendNewPlayerToDB(Bob);
 
         Match match = new Match(UWE, Page);
+
+        System.out.println(match.getHomeTeam().getPlayerList());
+        System.out.println(match.getAwayTeam().getPlayerList());
+
+        match.setHomeTeamPlayer1(Tom);
+        match.setHomeTeamPlayer2(Louis);
+        match.setAwayTeamPlayer1(Dave);
+        match.setAwayTeamPlayer2(Bob);
+
+        System.out.println(match.getMatchPlayers());
+
+        match.getSet(0).setHomeTeamPlayer(Tom);
+        match.getSet(0).setAwayTeamPlayer(Dave);
+
+        match.getSet(1).getGame(1).setHomeTeamScore(2);
+        match.getSet(1).getGame(1).setAwayTeamScore(1);
     }
 }

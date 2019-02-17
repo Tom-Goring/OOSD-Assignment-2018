112753a8487644e152cabc7f6f7ca03f132b9833
Tom Goring <tgoring97@gmail.com>
2019-02-17 17:47:54 +0000

    Add method to check if entered password of user account is correct

--- AssociationOrganiser/src/application/controller/LoginController.java
+++ AssociationOrganiser/src/application/controller/LoginController.java
@@ -1,19 +1,27 @@
 package application.controller;
 
+import DB.DatabaseManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.User;
+
 import java.io.IOException;
+import java.util.Arrays;
 
 public class LoginController {
 
     @FXML
     private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
 
     public void openCreateAccount(ActionEvent actionEvent) throws IOException {
 
@@ -25,10 +33,29 @@ public class LoginController {
         window.setScene(createAccountScene);
     }
 
+    public void openMainScene() {
 
+    }
 
     public void attemptLogIn(ActionEvent actionEvent) {
 
-        System.out.println(usernameField.getText());
+        // TODO: account for username non existence
+        User desiredUserAccount = DatabaseManager.User.loadUserUsingUsername(usernameField.getText());
+        String enteredPassword = passwordField.getText();
+
+        // hash entered password with desired user account salt
+        byte[] hashAttempt = DatabaseManager.Security.hashPassword(enteredPassword, desiredUserAccount.getSalt());
+
+        // compare hash of entered to pre-existing hash
+        if (Arrays.equals(hashAttempt, desiredUserAccount.getHashedPassword())) {
+
+            // login
+            System.out.println("success");
+        }
+        else {
+
+            // failure
+            System.out.println("wrong password");
+        }
     }
 }

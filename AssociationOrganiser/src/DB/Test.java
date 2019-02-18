286339f4c413cc84ed818f2814437838fb35f217
Tom Goring <tgoring97@gmail.com>
2019-02-18 16:25:18 +0000

    Test recent changes to player and team

--- AssociationOrganiser/src/DB/Test.java
+++ AssociationOrganiser/src/DB/Test.java
@@ -12,22 +12,18 @@ public class Test {
         DatabaseManager.createTables();
 
         Team UWE = new Team("UWE");
-        Team Page = new Team("Page");
-        Team Filton = new Team("Filton");
+        Player Tom = new Player("Tom");
+        Player Louis = new Player("Louis");
+        Player Tim = new Player("Tim");
 
-        Player Tom = new Player("Tom", UWE);
-        Player Louis = new Player("Louis", UWE);
-        Player Dave = new Player("Dave", Page);
-        Player Bob = new Player("Bob", Page);
+        DatabaseManager.Team.addNewTeamToDatabase(UWE);
+        DatabaseManager.Player.addNewPlayerToDatabase(Tom, UWE);
+        DatabaseManager.Player.addNewPlayerToDatabase(Louis, UWE);
+        DatabaseManager.Player.addNewPlayerToDatabase(Tim, UWE);
 
-        DatabaseManager.Team.insertTeam(UWE);
-        DatabaseManager.Team.insertTeam(Page);
-        DatabaseManager.Team.insertTeam(Filton);
+        Team test = DatabaseManager.Team.getTeamFromDatabase("UWE");
 
-        DatabaseManager.Player.sendNewPlayerToDB(Tom);
-        DatabaseManager.Player.sendNewPlayerToDB(Louis);
-        DatabaseManager.Player.sendNewPlayerToDB(Dave);
-        DatabaseManager.Player.sendNewPlayerToDB(Bob);
+        System.out.println(test.getPlayerList());
 
         Fixtures.generateFixtures();
     }
